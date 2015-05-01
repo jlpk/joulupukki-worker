1f4a5b3fc8529bb005a92813fd4520b803dbdb61
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2015-05-01 16:59:22 -0400

    Clean setup.cfg

--- joulupukki/worker/controllers/root.py
+++ joulupukki/worker/controllers/root.py
@@ -5,9 +5,6 @@ from webob.exc import status_map
 from joulupukki.worker.controllers.stats import StatsController
 
 
-from joulupukki.worker.controllers.v2 import v2
-
 
 class RootController(object):
-    v2 = v2.V2Controller()
     stats = StatsController()
