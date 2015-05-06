76b3de092bc71c08cda1b8523e26a958f3e7158f
Guillaume Auger <guillaume.auger@savoirfairelinux.com>
2015-05-06 15:33:35 -0400

    Using rsync instead of scp for logs files transmissions

--- joulupukki/worker/lib/osxpacker.py
+++ joulupukki/worker/lib/osxpacker.py
@@ -182,8 +182,8 @@ class OsxPacker(object):
             self.logger.error("Couldn't remove tmp job files: " + e)
 
         path = self.builder.origin_build_path + "/jobs/"
-        package_dir = self.builder.build.get_folder_path() + "/jobs/*"
-        transfert_command = "scp -r -i %s %s %s@%s:%s" % (
+        package_dir = self.job.get_folder_path() + "/"
+        transfert_command = 'rsync -az -e "ssh -i %s" %s %s@%s:%s' % (
             key,
             package_dir,
             user,
